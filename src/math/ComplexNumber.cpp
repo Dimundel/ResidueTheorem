e0caef40e19d996e45830e051a0fe23d72b1d630
Dimundel <deenixie@gmail.com>
2024-07-27 18:44:32 +0300

    add arg, addition, subtraction and multiplication for ComplexNumber

--- src/math/ComplexNumber.cpp
+++ src/math/ComplexNumber.cpp
@@ -4,3 +4,44 @@
 double abs(const ComplexNumber &number) {
   return std::sqrt(number.real * number.real + number.imag * number.imag);
 }
+
+double arg(const ComplexNumber &number) {
+  return std::atan2(number.imag, number.real);
+}
+
+ComplexNumber &operator+=(ComplexNumber &first, const ComplexNumber &second) {
+  first.real += second.real;
+  return first;
+}
+
+ComplexNumber operator+(const ComplexNumber &first,
+                        const ComplexNumber &second) {
+  ComplexNumber res = {first.real + second.real, first.imag + second.imag};
+  return res;
+}
+
+ComplexNumber &operator-=(ComplexNumber &first, const ComplexNumber &second) {
+  first.real -= second.real;
+  first.imag -= second.imag;
+  return first;
+}
+
+ComplexNumber operator-(const ComplexNumber &first,
+                        const ComplexNumber &second) {
+  ComplexNumber res = {first.real - second.real, first.imag + -second.imag};
+  return res;
+}
+
+ComplexNumber &operator*=(ComplexNumber &first, const ComplexNumber &second) {
+  double new_real = first.real * second.real - first.imag * second.imag;
+  first.imag = first.real * second.imag + first.imag * second.real;
+  first.real = new_real;
+  return first;
+}
+
+ComplexNumber operator*(const ComplexNumber &first,
+                        const ComplexNumber &second) {
+  ComplexNumber res = first;
+  res *= second;
+  return res;
+}
